035b4cec5e13c3fc73ce7ee7e9f710a53ec51d85
rezaul choudhury <rofy2009@gmail.com>
2022-05-22 03:27:12 +0100

    Given a year, return the century it is in...

---
+++ exercise_3.js
@@ -0,0 +1,14 @@
+// Given a year, return the century it is in.
+
+
+// The first century spans from the year 1 up to and including the year 100,
+// the second century - from the year 101 up to and including the year 200, etc.
+
+
+function century(year) {
+    // Finish this :)
+    let x = Math.ceil(year/ 100);
+    return x
+}
+console.log(century(1900))
+
