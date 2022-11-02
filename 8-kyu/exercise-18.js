cbebaa0e46e1524cb593c6c0e7ce268be3230bed
rezaul choudhury <rofy2009@gmail.com>
2022-11-02 18:52:06 +0000

    Invert values

--- 8-kyu/exercise-18.js
+++ 8-kyu/exercise-18.js
@@ -1,10 +1,29 @@
 /*
 Invert values
+
 DESCRIPTION:
-Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+Given a set of numbers, return the additive inverse of each.
+Each positive becomes negatives, and the negatives become positives.
 
 invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
 invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 You can assume that all values are integers. Do not mutate the input array/list.
  */
+
+
+function invert(arrays) {
+    let val = []
+    arrays.forEach(arr => {
+        if(arr >= 0){
+            val.push(Number('-'+arr))
+        } else if (arr <= 0) {
+            val.push(Math.abs(arr))
+        } else {
+            val.push([])
+        }
+    })
+    return val
+}
+
+console.log(invert([]));
