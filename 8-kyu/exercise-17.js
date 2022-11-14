363ddd57762dff642e7586c59e7d11389ad7ec3f
rezaul choudhury <rofy2009@gmail.com>
2022-11-14 17:56:33 +0000

    Find Multiples of a Number

--- 8-kyu/exercise-17.js
+++ 8-kyu/exercise-17.js
@@ -3,12 +3,45 @@
 Find Multiples of a Number
 
 DESCRIPTION:
-In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+In this simple exercise, you will build a program that takes a value, integer ,
+and returns a list of its multiples up to another value, limit . If limit is a multiple of integer,
+it should be included as well. There will only ever be positive integers passed into the function,
+not consisting of 0. The limit will always be higher than the base.
 
-For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+For example, if the parameters passed are (2, 5),
+the function should return [2, 4] as 2, 4, and 6 are the multiples of 2 up to 6.
 
 If you can, try writing it in only one line of code.
  */
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+
+
+
+
+// build a function
+// function take a value
+// integer
+// 10
+// 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+// 2, 4
+
+
+
+function findMultiples(integer, limit) {
+    //your code here
+    let result = []
+    let i = 1;
+    while (integer * i <= limit){
+        result.push(integer * i)
+        i += 1
+    }
+    return result
+}
+
+console.log(findMultiples(5, 25))
+
 
 
 
---
+++ 8-kyu/exercise-20.js
@@ -0,0 +1,16 @@
+/*
+Type of sum
+
+DESCRIPTION:
+    Return the type of the sum of the two arguments
+
+
+ */
+
+function typeOfSum(a, b) {
+    let result = a + b
+    // good luck
+    return result
+}
+
+console.log(typeof typeOfSum('s', false))
